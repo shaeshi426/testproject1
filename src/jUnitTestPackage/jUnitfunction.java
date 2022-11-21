fcdee10929cad5460332418521bb06a64f828634
admin <admin@10.12.47.82>
2022-11-21 17:03:09 +0530

    xy

---
+++ src/jUnitTestPackage/jUnitfunction.java
@@ -0,0 +1,12 @@
+package jUnitTestPackage;;
+
+public class jUnitfunction {
+	public int AddNumbers(int num1, int num2){
+        int ans = num1 + num2;
+        return ans;
+	}
+	public String AddStrings(String str1, String str2){
+        str1 = str1 + str2;
+        return str1;
+	}    
+}
